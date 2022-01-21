560ffb2a4c918d4e321ca6e1fed5bc8cb335e447
SRCinVA <scitarella@hotmail.com>
2022-01-20 19:48:07 -0500

    Updated the balance

--- acc.py
+++ acc.py
@@ -2,6 +2,7 @@
 class Account:
 
     def __init__(self, filepath):
+        self.class_filepath = filepath  # this makes filepath available anywhere in the class (notice the giveaway name)
         with open(filepath,'r') as file: 
             self.balance=int(file.read())  
             # this will save the value of the int from the text file in the instance variable self.balance.
@@ -13,7 +14,12 @@ class Account:
     def deposit(self, amount):  # both 'amount' variables are local, so they can have the same name. 
         self.balance = self.balance + amount
 
-account = Account("balance.txt") # for 'self,' Python will automatically pass the object instance.
-print(account.balance)   # uses dot notation to point to the object ("account") that holds the balance attribute 
-account.withdraw(100)  # the object instance is passed automatically, but we have to supply the amount.
-print(account.balance)  
+    def commit(self):
+        with open(self.class_filepath, 'w') as file:  # we open the filepath (see above) into to overwrite ('w') text file with updates. 
+            file.write(str(self.balance))
+
+my_account = Account("balance.txt") # for 'self,' Python will automatically pass the object instance. my_account is the object instance.
+print(my_account.balance)   # uses dot notation to point to the object ("account") that holds the balance attribute 
+my_account.withdraw(100)  # the object instance is passed automatically, but we have to supply the amount.
+print(my_account.balance)  
+my_account.commit() # don't need to pass any argument
