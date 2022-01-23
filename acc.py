558cfde4458a54b25ee01ffdc723704b93718742
SRCinVA <scitarella@hotmail.com>
2022-01-22 20:37:24 -0500

    Started with inheritance

--- acc.py
+++ acc.py
@@ -18,8 +18,21 @@ class Account:
         with open(self.class_filepath, 'w') as file:  # we open the filepath (see above) into to overwrite ('w') text file with updates. 
             file.write(str(self.balance))
 
-my_account = Account("balance.txt") # for 'self,' Python will automatically pass the object instance. my_account is the object instance.
-print(my_account.balance)   # uses dot notation to point to the object ("account") that holds the balance attribute 
-my_account.withdraw(100)  # the object instance is passed automatically, but we have to supply the amount.
-print(my_account.balance)  
-my_account.commit() # don't need to pass any argument
+class Checking(Account): # need to pass the base class in as an argument
+
+    def __init__(self, filepath):
+        Account.__init__(self, filepath)  # this creates a minimal object that inherits from the Account class
+                                            # you need to pass the same parameters in the parent and child lasses (it seeems)
+
+checking=Checking("balance.txt")
+checking.deposit(10)
+print(checking.balance)
+
+
+# remember CMD + / for multi-line comments
+
+# my_account = Account("balance.txt") # for 'self,' Python will automatically pass the object instance. my_account is the object instance.
+# print(my_account.balance)   # uses dot notation to point to the object ("account") that holds the balance attribute 
+# my_account.withdraw(100)  # the object instance is passed automatically, but we have to supply the amount.
+# print(my_account.balance)  
+# my_account.commit() # don't need to pass any argument
