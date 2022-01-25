0963a2eaf133ef232d93b5e94230eb6939dbfae7
SRCinVA <scitarella@hotmail.com>
2022-01-24 20:00:33 -0500

    small updates with definitions

--- acc.py
+++ acc.py
@@ -20,17 +20,22 @@ class Account:
 
 class Checking(Account): # need to pass the base class in as an argument
 
+    # an example of a doc string, which you can pull out with print(object.__doc__).
+    # They're very useful when you're importing classes from modules
+    """This class generates checking accoujnt objects"""
+
     type="checking" # a class variable is declared outside the methods contained by that class
                     # they are shared by all instances of that class
 
-    def __init__(self, filepath, fee):
-        Account.__init__(self, filepath)  # this creates a minimal object that inherits from the Account class
+    def __init__(self, filepath, fee):      # conventionally thought of as a "constructor"
+        Account.__init__(self, filepath)    # this creates a minimal object that inherits from the Account class
                                             # you need to pass the same parameters in the parent and child lasses (it seeems)
         self.fee = fee                      # Transform this fee into an instance variable
 
     def transfer(self, amount, fee):
         self.balance = self.balance - amount - self.fee
 
+# these two below are both "instantiation":
 jack_checking=Checking("jack.txt", 2) # it seems self is automatically passed as the instance is created
 jack_checking.transfer(20,2)
 print(jack_checking.balance)  # prints out the balance instance variable of the base Class
@@ -41,7 +46,6 @@ john_checking=Checking("john.txt", 2) # it seems self is automatically passed as
 john_checking.transfer(10,2)
 print(john_checking.balance)  # prints out the balance instance variable of the base Class
 john_checking.commit() 
-
 
 # remember CMD + / for multi-line comments
 
